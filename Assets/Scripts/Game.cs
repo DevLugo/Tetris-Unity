d175c731e9f78d8d2c064bee6b8aede2b3a61c09
DevLugo <bl1ndcod3@gmail.com>
2020-01-19 16:57:23 -0500

    first commit/functional random form

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public static int gridWidth = 10;
+    public static int gridHeight = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnNextTetromino();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnNextTetromino(){
+        GameObject newTetromino = (GameObject)Instantiate(Resources.Load(getRandomTetromino(), typeof(GameObject)), new Vector2(5.0f,20.0f), Quaternion.identity);
+
+    }
+
+    public bool CheckIsInsideGrid(Vector2 pos)
+    {
+        return ((int)pos.x >= 0 && (int)pos.x < gridWidth && (int)pos.y >= 0);
+    }
+
+    public Vector2 Round(Vector2 pos)
+    {
+        return new Vector2(Mathf.Round(pos.x), Mathf.Round(pos.y));
+    }
+
+    string getRandomTetromino(){
+        int randomTetromino = Random.Range(1,8);
+        string randomTetrominoName = "Prefabs/Tetromino_T";
+
+        switch (randomTetromino)
+        {
+            case 1:
+                randomTetrominoName = "Prefabs/Tetromino_T";
+                break;
+            case 2:
+                randomTetrominoName = "Prefabs/Tetromino_Long";
+                break;
+            case 3:
+                randomTetrominoName = "Prefabs/Tetromino_Square";
+                break;
+            case 4:
+                randomTetrominoName = "Prefabs/Tetromino_j";
+                break;
+            case 5:
+                randomTetrominoName = "Prefabs/Tetromino_L";
+                break;
+            case 6:
+                randomTetrominoName = "Prefabs/Tetromino_S";
+                break;
+            case 7:
+                randomTetrominoName = "Prefabs/Tetromino_Z";
+                break;
+        }
+        return randomTetrominoName;
+    }
+}
